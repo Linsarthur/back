58e32bfc7f66cc8e46f8a9c4c2fb2e3466bc2c74
Arthur Cavalcante <linsarthur02@gmail.com>
2025-08-29 20:52:10 -0300

    users controller and routes created

--- src/controllers/usersControllers.js
+++ src/controllers/usersControllers.js
@@ -1,8 +1,65 @@
 import { prisma } from "../services/index.js";
-export async function getUsuarios() {
+export async function getUsers() {
     try {
         return await prisma.user.findMany();
     } catch (error) {
         return error.message;
+    }
+}
+
+export async function getUserById(id) {
+    try {
+        return await prisma.user.findUnique({
+            where: { user_id: Number(id) }
+        });
+    } catch (error) {
+        return error.message;
+    }
+}
+
+export async function createUser(req, res) {
+    try {
+        const { user_name, user_password, user_email } = req.body;
+        const user = await prisma.user.create({
+            data: {
+                user_name,
+                user_password,
+                user_email
+            }
+        });
+        return res.status(201).json(user);
+    } catch (error) {
+        return res.status(400).json({ error: error.message });
+    }
+}
+export async function editUser(req, res) {
+    try {
+        const { id } = req.params
+        const { user_name, user_password, user_email } = req.body;
+        const user = await prisma.user.update({
+            where: { user_id: Number(id) },
+            data: {
+                user_name,
+                user_password,
+                user_email
+            }
+        });
+        return res.status(201).json(user);
+    } catch (error) {
+        return res.status(400).json({ error: error.message });
+    }
+}
+
+export async function deleteUser(req, res) {
+    try {
+        const { id } = req.params;
+       const user = await prisma.user.delete({
+            where: { user_id: Number(id) }
+        });
+        return res.status(200).json({message: "User deleted successfully",
+            user
+        });
+    } catch (error) {
+        return res.status(400).json({ error: error.message });
     }
 }
--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -1,11 +1,20 @@
 import { Router } from "express";
-import { getUsuarios } from "../controllers/usersControllers.js";
+import { createUser, deleteUser, editUser, getUserById, getUsers } from "../controllers/usersControllers.js";
 
 const router = Router();
 
 router.get('/', async (req, res) => {
-    res.send(await getUsuarios());
+    res.send(await getUsers());
 })
 
+router.get('/:id', async (req, res) => {
+    const { id } = req.params;
+    res.send(await getUserById(id));
+})
+
+router.post('/', createUser);
+router.put('/:id', editUser);
+router.delete('/:id',deleteUser);
 
 export { router as usersRoutes };
+
